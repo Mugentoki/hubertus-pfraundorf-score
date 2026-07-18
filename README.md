a66b3314977e9bdcc8e87db79c24b2031288fd63
Mugentoki <alexberger@freenet.de>
2026-07-18 21:15:19 +0200

    add some basic styles, think about workflow

--- README.md
+++ README.md
@@ -1,7 +1,26 @@
-# Tauri + Vue + TypeScript
+# Hubertus - DISAG Scoring Software
+
+A simple software to evaluate DISAG Opticscore XML files
+
+## Dev Notes
+
+### Workflow:
+1. App starts with "Drag and drop" field to let the user load the XML file
+2. After adding it, the app converts the XML to JSON and normalises it -- removes data that's not needed, maybe calculate some scores that are always needed and so on
+3. After normalising, the user can select filters, sortings, calculations and so on
+4. The selected options from point 3 are run through a pipeline, each selection will then mutate a copy of the original JSON data
+5. The final score is displayed on the screen (even live update possible while user selects options in point 3)
+
+Make sure that the pipeline doesn't know which mutations need to be done, it just runs them.
+-> makes it easier to add new mutations
+-> mutations use the same interface
+
+## Development
+
+### Tauri + Vue + TypeScript
 
 This template should help get you started developing with Vue 3 and TypeScript in Vite. The template uses Vue 3 `<script setup>` SFCs, check out the [script setup docs](https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup) to learn more.
 
-## Recommended IDE Setup
+### Recommended IDE Setup
 
 - [VS Code](https://code.visualstudio.com/) + [Vue - Official](https://marketplace.visualstudio.com/items?itemName=Vue.volar) + [Tauri](https://marketplace.visualstudio.com/items?itemName=tauri-apps.tauri-vscode) + [rust-analyzer](https://marketplace.visualstudio.com/items?itemName=rust-lang.rust-analyzer)
--- src/App.vue
+++ src/App.vue
@@ -27,5 +27,157 @@ const currentView = computed(() => {
 })
 </script>
 
-<style>
+<style block="VARIABLES">
+:root {
+  --app-background-color: #224e00;
+  --app-background-image: url('/src/assets/images/background.png');
+
+  --default-font-family: 'Lato', sans-serif;
+  --default-text-color: #ffffff;
+}
+</style>
+
+<style block="RESET">
+/*
+  1. Use a more-intuitive box-sizing model.
+*/
+*, *::before, *::after {
+  box-sizing: border-box;
+}
+/*
+  2. Remove default margin
+*/
+* {
+  margin: 0;
+}
+
+/*
+  4. Allow percentage-based heights in the application
+*/
+html, body {
+  height: 100%;
+  scroll-behavior: smooth;
+
+  @media all and (min-width: 768px) {
+    scroll-padding-top: 75px;
+  }
+}
+/*
+  Typographic tweaks!
+  5. Add accessible line-height
+  6. Improve text rendering
+*/
+body {
+  line-height: 1.5;
+  -webkit-font-smoothing: antialiased;
+}
+/*
+  7. Improve media defaults
+*/
+img, picture, video, canvas, svg {
+  display: block;
+  max-width: 100%;
+}
+/*
+  8. Remove built-in form typography styles
+*/
+input, button, textarea, select {
+  font: inherit;
+}
+/*
+  9. Avoid text overflows
+*/
+p, h1, h2, h3, h4, h5, h6 {
+  overflow-wrap: break-word;
+}
+/*
+  10. Create a root stacking context
+*/
+#app {
+  isolation: isolate;
+}
+</style>
+
+<style block="TYPOGRAPHY">
+@font-face {
+  font-family: "Lato";
+  font-weight: 400;
+  font-style: normal;
+  font-display: swap;
+  unicode-range: U+000-5FF;
+  src: local("Noto Serif"), url("/src/assets/fonts/lato/Lato-Regular.woff2") format("woff2");
+}
+
+@font-face {
+  font-family: "Lato";
+  font-weight: 700;
+  font-style: normal;
+  font-display: swap;
+  unicode-range: U+000-5FF;
+  src: local("Noto Serif"), url("/src/assets/fonts/lato/Lato-Bold.woff2") format("woff2");
+}
+
+body {
+  font-family: var(--default-font-family);
+  font-weight: 400;
+  color: var(--default-text-color);
+  font-size: 1.6rem;
+  word-wrap: break-word;
+  hyphens: auto;
+}
+
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  font-weight: 700;
+}
+
+h1,
+.h1 {
+  font-size: 2.8rem;
+
+  @media all and (min-width: 768px) {
+    font-size: 2.8rem;
+  }
+}
+
+h2,
+.h2 {
+  font-size: 3.2rem;
+
+  @media all and (min-width: 768px) {
+    font-size: 3.8rem;
+  }
+}
+
+h3,
+.h3 {
+  font-size: 2.4rem;
+}
+
+h4,
+.h4 {
+  font-size: 2rem;
+}
+
+</style>
+
+<style block="APP STYLES">
+html, body, #app {
+  margin: 0;
+  padding: 0;
+  height: 100dvh;
+  width: 100dvw;
+}
+
+#app {
+  background-color: var(--app-background-color);
+  background-image: var(--app-background-image);
+  background-size: 100px;
+  color: var(--default-text-color);
+}
 </style>
